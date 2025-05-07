0f7daaff871628e22a06b61680671b397ad46820
prodante <ya.dante8@yandex.ru>
2025-05-07 10:07:27 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import HomePage from './components/home/HomePage';
+import Header from './components/widget/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <HomePage/>
+    </div>
+  );
+}
+
+export default App;
